7715ccdcb55b340da5e22a282285a5b076408133
sedamcheon <sedamcheon@gmail.com>
2026-06-10 21:38:22 +0900

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,5 @@
+window.CAFE_FIREBASE_CONFIG = {
+  apiKey: "PASTE_FIREBASE_WEB_API_KEY",
+  projectId: "cafe-aef0b",
+  databaseURL: "https://cafe-aef0b-default-rtdb.firebaseio.com"
+};
---
+++ firebase-config.js
@@ -0,0 +1,5 @@
+window.CAFE_FIREBASE_CONFIG = {
+  apiKey: "여기에_FIREBASE_WEB_API_KEY_입력",
+  projectId: "cafe-aef0b",
+  databaseURL: "https://cafe-aef0b-default-rtdb.firebaseio.com"
+};
